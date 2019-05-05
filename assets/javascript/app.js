4959d2438b443779a46a18ee802cf73535a2615a
aancjr <45945137+aancjr@users.noreply.github.com>
2019-05-05 18:31:54 -0400

    added table layout in schedule, firebase information and quick CSS

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAJ6ITLDJxMa6eAHw3hwbmouQykBnXbv0Q",
+    authDomain: "train-schedule-f9e26.firebaseapp.com",
+    databaseURL: "https://train-schedule-f9e26.firebaseio.com",
+    projectId: "train-schedule-f9e26",
+    storageBucket: "train-schedule-f9e26.appspot.com",
+    messagingSenderId: "65747457306",
+    appId: "1:65747457306:web:e0ea84cbf23d6ffe"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
