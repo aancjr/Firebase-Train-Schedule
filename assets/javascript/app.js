d7768a64b386db1f887690922a3c2e087b331044
aancjr <45945137+aancjr@users.noreply.github.com>
2019-05-10 21:14:41 -0400

    added quick comments in JS to let reader know the use of each section/line

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,53 +11,63 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-
+//variable set to access the beginning string of the firebase call
 var database = firebase.database();
 
+//function being called when submit is hit
 $("#submit").on("click", function(event){
+    //preventing the page to jump to the top, but perform the function
     event.preventDefault();
 
+    //variables set to grab the users input in the form
     var train = $("#train-name").val();
     var place = $("#destination").val();
     var time = $("#first-train-time").val();
     var frequency = $("#frequency").val();
 
-    console.log(train, place, time, frequency);
-
+    //creating the key and variable in firebase. since they're named the same, we can use "," instead of duplicating text.
     database.ref().push({
         train,
         place,
         time,
         frequency
     });
-
+    
+    //reset the form input boxes
     $("form").trigger("reset");
 });
 
+//using childadded to retrieve from firebase
 database.ref().on("child_added", function(childSnapshot){
-    console.log(childSnapshot.val());
 
+    //variables created to pull the value from the right key in firebase
     var trainStamp = childSnapshot.val().train;
     var placeStamp = childSnapshot.val().place;
     var timeStamp = childSnapshot.val().time;
     var frequencyStamp = childSnapshot.val().frequency;
 
+    //variable to indicate the time given
     var convertedTime = moment(timeStamp, "HH:mm");
-    var minutesAway = (convertedTime.toNow());
 
-    console.log(moment().toNow)
+    // var minutesAway = (convertedTime.toNow());
 
+    //calculating the difference in time from the given form time to the current time and in minutes
     var timeDiff = moment().diff(moment(convertedTime), "minutes")
 
+    //using a modular to find out what time apart is by getting the remainder
     var tRemainder = timeDiff % frequencyStamp;
 
+    //calculate minutes until next train
     var tMinutesUntil = frequencyStamp - tRemainder;
 
+    //Next train time
     var nextTrain = moment().add(tMinutesUntil, "minutes");
     
+    //convert next train tine to arrival time
     var nextArrival = moment(nextTrain).format("HH:mm");
     console.log(tMinutesUntil)
 
+    //table created with each piece of data ready to be created
     var newRow = $("<tr>").append(
         $("<td>").text(trainStamp),
         $("<td>").text(placeStamp),
@@ -66,6 +76,7 @@ database.ref().on("child_added", function(childSnapshot){
         $("<td>").text(tMinutesUntil),
     )
 
+    //table being created and appending a new row with each user input
     $("#train-table").append(newRow);
 
 })
